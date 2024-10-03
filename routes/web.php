f7b53ce1d14cd19bd282640a4771f222b987fa2d
SchuGabriel <schu-gabriel@hotmail.com>
2024-10-03 20:51:23 -0300

    criado controller e view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/quiz', [QuizController::class, 'index'])->name('quiz.index');
